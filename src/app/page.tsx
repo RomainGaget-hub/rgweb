30a8d6d06a02161a69e0ab2a740f3ea8b5549d8f
Romain <romaingaget@gmail.com>
2024-10-20 13:50:19 +0100

    header logo and Profile styling with picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,32 @@
-'use client'
+'use client';
 
 // pages/index.tsx
 import React, { useState, useEffect } from 'react';
-import Profile from './components/Profile';
+import Profile from '../components/Profile';
 import BlogPreview from './components/BlogPreview';
 // Import CSS
 import './styles/theme.css';
 import Skills from './components/Skills';
 
 const Home = () => {
-  const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState('light');
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      setTheme(savedTheme);
-    }
-  }, []);
+	useEffect(() => {
+		const savedTheme = localStorage.getItem('theme');
+		if (savedTheme) {
+			setTheme(savedTheme);
+		}
+	}, []);
 
-
-
-  return (
-    <section className='py-24'>
-      <div className="container max-w-3xl">
-        <Profile />
-        <Skills />
-        <BlogPreview />
-      </div>
-    </section>
-  );
+	return (
+		<section className='py-24'>
+			<div className='container max-w-3xl'>
+				<Profile />
+				<Skills />
+				<BlogPreview />
+			</div>
+		</section>
+	);
 };
 
 export default Home;
