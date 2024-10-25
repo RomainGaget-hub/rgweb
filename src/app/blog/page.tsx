646d2640c51e945a40b7a41c3ea6896569716949
Romain <romaingaget@gmail.com>
2024-10-25 17:04:46 +0100

    form for new post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,22 +3,39 @@ import prisma from '@/lib/db';
 import Link from 'next/link';
 
 const Page = async () => {
-    const posts = await prisma.post.findMany();
-    return (
-        <div className='container flex gap-2 justify-center items-center flex-col content-center'>
-            <h1 className='py-5 font-bold'>Latest Post</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link href={`/blog/${post.slug}`}>
-                            {post.title}
-                        </Link>
-
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+	const posts = await prisma.post.findMany();
+	return (
+		<div className='container flex flex-col content-center items-center justify-center gap-2'>
+			<h1 className='py-5 font-bold'>Latest Post</h1>
+			<ul className='my-20 border-slate-400'>
+				{posts.map((post) => (
+					<li key={post.id}>
+						<Link href={`/blog/${post.slug}`}>{post.title}</Link>
+					</li>
+				))}
+			</ul>
+			<form className='mx-auto flex w-[300px] flex-col gap-y-2'>
+				<input
+					type='text'
+					name='title'
+					placeholder='Title'
+					className='rounded-sm px-2 py-1'
+				/>
+				<textarea
+					name='content'
+					rows={5}
+					placeholder='Content'
+					className='rounded-sm px-2 py-1'
+				/>
+				<button
+					type='submit'
+					className='rounded-sm bg-blue-500 px-2 py-1 text-white'
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default Page;
