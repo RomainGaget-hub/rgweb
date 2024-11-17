8c73d365d3fa1f355440744708cdab9d1ed25496
Romain <romaingaget@gmail.com>
2024-11-17 17:00:16 +0000

    admin blog posts table

--- src/app/admin/blogpost/page.jsx
+++ src/app/admin/blogpost/page.jsx
@@ -5,10 +5,22 @@ import Link from 'next/link';
 import {
 	Table,
 	TableBody,
+	TableCell,
 	TableHead,
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table';
+import prisma from '@/lib/db';
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuSubContent,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export default function AdminBlogPost() {
 	return (
@@ -24,7 +36,23 @@ export default function AdminBlogPost() {
 	);
 }
 
-function BlogPostsTable() {
+async function BlogPostsTable() {
+	const blogPosts = await prisma.post.findMany({
+		select: {
+			id: true,
+			title: true,
+			content: true,
+			slug: true,
+			excerpt: true,
+			published: true,
+			createdAt: true,
+		},
+		orderBy: { createdAt: 'desc' },
+	});
+
+	if (!blogPosts) {
+		return <div>No blog posts found.</div>;
+	}
 	return (
 		<Table>
 			<TableHeader>
@@ -42,7 +70,46 @@ function BlogPostsTable() {
 					</TableHead>
 				</TableRow>
 			</TableHeader>
-			<TableBody></TableBody>
+			<TableBody>
+				{blogPosts.map((blogPost) => (
+					<TableRow key={blogPost.id}>
+						<TableCell className='whitespace-nowrap px-6 py-4'>
+							{blogPost.published ? (
+								<>
+									<CheckCircle2 />
+									<span className='sr-only'>Published</span>
+								</>
+							) : (
+								<>
+									<XCircle />
+									<span className='sr-only'>Not Published</span>
+								</>
+							)}
+						</TableCell>
+						<TableCell>{blogPost.title}</TableCell>
+						<TableCell>{blogPost.slug}</TableCell>
+						<TableCell>{blogPost.content}</TableCell>
+						<TableCell>{blogPost.excerpt}</TableCell>
+						<TableCell>Author</TableCell>
+						<TableCell>
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<MoreVertical />
+									<span className='sr-only'>Actions</span>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuLabel>Actions</DropdownMenuLabel>
+									<DropdownMenuItem>
+										<Link href={`/admin/blogpost/${blogPost.id}`}>Edit</Link>
+									</DropdownMenuItem>
+									<DropdownMenuSeparator />
+									<DropdownMenuItem>Delete</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
 		</Table>
 	);
 }
